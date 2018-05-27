02055d9fbd08a37eeacc1c07dd8f8d63484ebc14
Nikolay Bezdetniy <nekooolay@mail.ru>
2018-05-27 13:49:52 +0300

    Caesar

--- Caesar/Caesar/Program.cs
+++ Caesar/Caesar/Program.cs
@@ -17,6 +17,7 @@ namespace Caesar
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
